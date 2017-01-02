c52e8b1448852502abfba86e84085562f762e832
toshi0607 <s.toshi0607@gmail.com>
2017-01-02 10:16:44 +0900

    add js file

---
+++ ch04/radioinput.js
@@ -0,0 +1,33 @@
+var RadioButton = React.createClass({
+  render: function() {
+    return (<div>どれか１つを選んでください:
+      <div>
+        <input
+          type='radio'
+          name='三択'
+          value='first'
+          checked
+        />最初の選択肢
+      </div>
+      <div>
+        <input
+          type='radio'
+          name='三択'
+          value='second'
+        />次の選択肢
+      </div>
+      <div>
+        <input
+          type='radio'
+          name='三択'
+          value='third'
+        />最後の選択肢
+      </div>
+    </div>);
+  }
+});
+
+ReactDOM.render(
+  <RadioButton />,
+  document.getElementById('content')
+);
