913702f132816b0536c85100d1aab3b78d17c5c4
toshi0607 <s.toshi0607@gmail.com>
2017-01-02 09:58:06 +0900

    make component controlable

--- ch04/textinput.js
+++ ch04/textinput.js
@@ -1,6 +1,18 @@
 var TextInput = React.createClass({
+  getInitialState: function() {
+    return {dispText: '入力欄'};
+  },
+  handleInput: function(event) {
+    this.setState({dispText: event.target.value});
+    console.log(this.state.dispText);
+  },
   render: function() {
-    return (<div>JSX Input: <input type='text' defaultValue='入力欄' /></div>);
+    return (<div>JSX InputText:
+      <input type='text'
+       value={this.state.dispText}
+       onChange={this.handleInput}
+     />
+    </div>);
   }
 });
 
