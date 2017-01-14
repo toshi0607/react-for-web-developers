d0631c15423216a59fc2dc903d9051fa22fb7923
toshi0607 <s.toshi0607@gmail.com>
2017-01-14 13:01:22 +0900

    add js

---
+++ ch05/02/transform.js
@@ -0,0 +1,36 @@
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+var CheckAnimate = React.createClass({
+  getInitialState: function() {
+    return {checked: false,
+            visibleText: ''};
+  },
+  checkChange: function(event) {
+    if (event.target.checked) {
+      this.setState({visibleText: '普段は見えないテキスト'})
+    } else {
+      this.setState({visibleText: ''})
+    }
+    this.setState({checked: event.target.checked});
+  },
+  render: function() {
+    return ( <div>
+      <input
+        type="checkbox"
+        checked={this.state.checked}
+        onChange={this.checkChange}
+      />隠れたテキストを表示
+      <ReactCSSTransitionGroup
+        transitionName="fadingText"
+        transitionEnterTimeout={1500}
+        transitionLeaveTimeout={500}>
+        <h2 key={this.state.checked}>{this.state.visibleText}</h2>
+      </ReactCSSTransitionGroup>
+    </div>);
+  }
+});
+
+ReactDOM.render(
+  <CheckAnimate />,
+  document.getElementById('content')
+);
